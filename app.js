226ed1a984c5262ae802e0eba13e1fa4cd60c899
Pavel Mareev <etpotato@80-78-245-19.cloudvps.regruhosting.ru>
2021-05-18 15:12:18 +0300

    Add working server + tgbot

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const TelegramBot = require('node-telegram-bot-api');
+const path = require('path');
 const express = require('express');
+const app = express();
+const multer  = require('multer');
+const upload = multer();
 
 const TOKEN = '1855434521:AAH8yD4eNlFvDx0qprTYFs3tZ0EuamRqeqM';
 const bot = new TelegramBot(TOKEN, {polling: true});
 const TG_CHAT_ID = -514747166;
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-  bot.sendMessage(TG_CHAT_ID, 'test');
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId,
+    '<b>Я работаю. Новых заказов нет.',
+    {parse_mode: 'HTML'});
 });
 
-app.listen(5000, () => {
-  console.log('app listening on port 5000');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/', upload.none(), (req, res) => {
+  bot.sendMessage(TG_CHAT_ID,
+    '<b>Новый заказ: </b>'+req.body.tel,
+    {parse_mode : 'HTML'})
+   .then(()=> res.status(200).send('Номер телефона отправлен. Ожидайте звонка.'))
+   .catch((error) => res.status(400).send('Ошибка отправки. Попробуйте еще раз.'));
 });
+
+app.listen(5000);
